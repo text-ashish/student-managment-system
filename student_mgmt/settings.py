b794eae7457da4dae8dae8ee71670a9fde3fd912
Ashish Srivastava <98154274+ShoutToLearnorg@users.noreply.github.com>
2025-07-11 11:47:24 +0530

    Set STATIC_ROOT for collectstatic

--- student_mgmt/settings.py
+++ student_mgmt/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4g8b3ri%4bekni=7-@wsp7!(ti(@c@dse)-(0_0pxq!1xc(*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_URL = '/static/'
 
 # Application definition
 
